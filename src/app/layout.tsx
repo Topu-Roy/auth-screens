966b6ce6ade4365f9183c2d6095309978ef45df9
Topu-Roy <topuroy143333@gmail.com>
2024-09-13 15:46:13 +0600

    Fix - Empty import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@/styles/globals.css";
-import "";
 
 const geistSans = localFont({
   src: "../styles/fonts/GeistVF.woff",
